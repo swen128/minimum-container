d967e5b41b08259fc38039a9e8603531d6fe9a80
PND <fujjisaaan@gmail.com>
2020-12-09 10:52:47 +0900

    namespace 設定後の初期化処理を追加

--- main.go
+++ main.go
@@ -11,19 +11,20 @@ import (
 func main() {
 	switch os.Args[1] {
 	case "run":
+		initialize(os.Args[2:]...)
+	case "child":
 		execute(os.Args[2], os.Args[3:]...)
 	default:
 		panic("コマンドライン引数が正しくありません。")
 	}
 }
 
-// 隔離されたプロセスの中で cmd を引数 arg と共に実行
-func execute(cmd string, args ...string) {
-	// ルートディレクトリとカレントディレクトリを ./rootfs に設定
-	must(unix.Chroot("./rootfs"))
-	must(unix.Chdir("/"))
+// Linux namespace を設定した子プロセスで、execute 関数を実行する
+func initialize(args ...string) {
+	// このプログラム自身に引数 child <cmd> <args> を渡す
+	arg := append([]string{"child"}, args...)
+	command := exec.Command("/proc/self/exe", arg...)
 
-	command := exec.Command(cmd, args...)
 	command.Stdin = os.Stdin
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
@@ -31,6 +32,23 @@ func execute(cmd string, args ...string) {
 	command.SysProcAttr = &unix.SysProcAttr{
 		Cloneflags: unix.CLONE_NEWNS | unix.CLONE_NEWPID | unix.CLONE_NEWUTS,
 	}
+
+	must(command.Run())
+}
+
+// namespace 設定後の初期化処理と、ユーザー指定のコマンドを実行する
+func execute(cmd string, args ...string) {
+	// ルートディレクトリとカレントディレクトリを ./rootfs に設定
+	must(unix.Chroot("./rootfs"))
+	must(unix.Chdir("/"))
+
+	must(unix.Mount("proc", "proc", "proc", 0, ""))
+	must(unix.Sethostname([]byte("my-container")))
+
+	command := exec.Command(cmd, args...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
 
 	must(command.Run())
 }
